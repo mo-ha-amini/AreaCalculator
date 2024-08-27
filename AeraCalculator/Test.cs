97fdd980942e0545f0a7dc2d34d9a548ac53e9d6
moha_amini <mhaminii18@gmail.com>
2024-08-27 15:43:38 +0330

    add test class to program

---
+++ AeraCalculator/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AeraCalculator
+{
+    public class Test
+    {
+        public void Run()
+        {
+            IAreaStrategy shoelaceStrategy = new ShoelaceAreaStrategy();
+            AreaCalculator calculator = new AreaCalculator(shoelaceStrategy);
+
+            Console.WriteLine("Enter the number of coordinates:");
+            int numCoordinates = int.Parse(Console.ReadLine());
+
+            List<(double x, double y)> coordinates = new List<(double x, double y)>();
+            for (int i = 0; i < numCoordinates; i++)
+            {
+                Console.WriteLine($"Enter x-coordinate for point {i + 1}:");
+                double x = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Enter y-coordinate for point {i + 1}:");
+                double y = double.Parse(Console.ReadLine());
+
+                coordinates.Add((x, y));
+            }
+
+            double area = calculator.CalculateArea(coordinates);
+            Console.WriteLine($"Area: {area}");
+        }
+    }
+}
